a205cb24960870e297a7be66f946815f30b60a06
Jose Naime <josep.naimeg@gmail.com>
2021-07-24 16:28:34 -0500

    Example Questions added in file

--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -1,4 +1,81 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+// Only testing purpose, delete for final build.
+const QUESTIONS = [
+    {
+        id: 0,
+        text: "Question 1",
+        options: [
+            {
+                text: "Answer 1"
+            },
+            {
+                text: "Answer 2"
+            },
+            {
+                text: "Answer 3"
+            },
+            {
+                text: "Answer 4"
+            },
+
+        ]
+    }, {
+        id: 1,
+        text: "Question 2",
+        options: [
+            {
+                text: "Answer 1"
+            },
+            {
+                text: "Answer 2"
+            },
+            {
+                text: "Answer 3"
+            },
+            {
+                text: "Answer 4"
+            },
+
+        ]
+    }, {
+        id: 2,
+        text: "Question 3",
+        options: [
+            {
+                text: "Answer 1"
+            },
+            {
+                text: "Answer 2"
+            },
+            {
+                text: "Answer 3"
+            },
+            {
+                text: "Answer 4"
+            },
+
+        ]
+    }, {
+        id: 3,
+        text: "Question 4",
+        options: [
+            {
+                text: "Answer 1"
+            },
+            {
+                text: "Answer 2"
+            },
+            {
+                text: "Answer 3"
+            },
+            {
+                text: "Answer 4"
+            },
+
+        ]
+    },
+];
 
 function QuestionCard(props) {
     return (
