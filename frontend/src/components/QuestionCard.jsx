4874b78482f855572968e328ce8b84c0dd5a30c6
Jose Naime <josep.naimeg@gmail.com>
2021-07-24 16:13:30 -0500

    feat(Component): QuestionCard created

---
+++ frontend/src/components/QuestionCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function QuestionCard(props) {
+    return (
+        <div>
+            <h1>Question Card</h1>
+        </div>
+    );
+}
+
+export default QuestionCard;
