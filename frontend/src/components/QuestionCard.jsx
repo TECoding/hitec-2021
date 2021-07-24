b15502ffad5c0ceec3bc72f5fa8c84a125aca992
Jose Naime <josep.naimeg@gmail.com>
2021-07-24 17:01:59 -0500

    Component restructured, basic navigation functions added

--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -19,7 +19,8 @@ const QUESTIONS = [
                 text: "Answer 4"
             },
 
-        ]
+        ],
+        answerIndex: null,
     }, {
         id: 1,
         text: "Question 2",
@@ -37,7 +38,8 @@ const QUESTIONS = [
                 text: "Answer 4"
             },
 
-        ]
+        ],
+        answerIndex: null
     }, {
         id: 2,
         text: "Question 3",
@@ -55,7 +57,8 @@ const QUESTIONS = [
                 text: "Answer 4"
             },
 
-        ]
+        ],
+        answerIndex: null,
     }, {
         id: 3,
         text: "Question 4",
@@ -73,14 +76,55 @@ const QUESTIONS = [
                 text: "Answer 4"
             },
 
-        ]
+        ],
+        answerIndex: null,
     },
 ];
 
-function QuestionCard(props) {
+function QuestionCard() {
+    const [currentStep, setCurrentStep] = useState(0);
+
+    const handleQuestionsNavClick = (ev) => {
+        setCurrentStep(ev.target.value)
+    }
+
+    const handleNextQuestionClick = () => {
+        // TODO: Check if last item
+
+        setCurrentStep(currentStep + 1);
+    }
+
+    const handlePrevQuestionClick = () => {
+        // TODO: Check if first item
+
+        setCurrentStep(currentStep - 1);
+    }
+
+
+    const questionsNav = QUESTIONS.map((question, i) => {
+        return (
+            <button onClick={handleQuestionsNavClick} value={question.id}>
+                <p>{i+1}</p>
+            </button>
+        )
+    })
+
+    const currentQuestion = QUESTIONS.filter(question => {
+        return question.id === currentStep
+    })
+
     return (
         <div>
-            <h1>Question Card</h1>
+            <div>
+                {questionsNav}
+            </div>
+            <div>
+                <h2>{currentQuestion.text}</h2>
+            </div>
+            <div>
+                <p onClick={handlePrevQuestionClick}>Anterior</p>
+                <p onClick={handleNextQuestionClick}>Siguiente</p>
+            </div>
         </div>
     );
 }
