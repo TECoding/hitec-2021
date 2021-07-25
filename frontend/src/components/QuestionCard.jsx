ed07287bc94dd4952732a2fc5bbcd5a2682c8b11
Jose Naime <josep.naimeg@gmail.com>
2021-07-24 21:39:28 -0500

    Navigation handles first and last item

--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -84,46 +84,63 @@ const QUESTIONS = [
 function QuestionCard() {
     const [currentStep, setCurrentStep] = useState(0);
 
+    const currentQuestion = QUESTIONS[currentStep];
+
+    // HANDLERS
     const handleQuestionsNavClick = (ev) => {
         setCurrentStep(ev.target.value)
+        console.log(ev.target.value)
+    }
+
+    const handleOptionClick = (ev) => {
+        currentQuestion.answerIndex = ev.target.value;
     }
 
     const handleNextQuestionClick = () => {
-        // TODO: Check if last item
-
         setCurrentStep(currentStep + 1);
     }
 
     const handlePrevQuestionClick = () => {
-        // TODO: Check if first item
-
         setCurrentStep(currentStep - 1);
     }
 
-
     const questionsNav = QUESTIONS.map((question, i) => {
         return (
-            <button onClick={handleQuestionsNavClick} value={question.id}>
-                <p>{i+1}</p>
+            <button key={"question_"+ i +"_nav"} onClick={handleQuestionsNavClick} value={i}>
+                {i + 1}
             </button>
         )
     })
 
-    const currentQuestion = QUESTIONS.filter(question => {
-        return question.id === currentStep
+    const questionOptions = currentQuestion.options.map((option, i) => {
+        return(
+            <button key={"option_" + i} value={i} onClick={handleOptionClick}>
+                {currentQuestion.id + " " + option.text}
+            </button>
+        )
     })
 
     return (
         <div>
+            {/*--NAV--*/}
             <div>
                 {questionsNav}
             </div>
+            {/*--QUESTION--*/}
             <div>
-                <h2>{currentQuestion.text}</h2>
+                {/*QUESTION --TEXT--*/}
+                <div>
+                    <h2>{currentQuestion.text}</h2>
+                </div>
+                {/*QUESTION --OPTIONS--*/}
+                <div>
+                    {questionOptions}
+                </div>
             </div>
+            {/*--BUTTONS--*/}
             <div>
-                <p onClick={handlePrevQuestionClick}>Anterior</p>
-                <p onClick={handleNextQuestionClick}>Siguiente</p>
+                {currentStep !== 0 ? <p onClick={handlePrevQuestionClick}>Anterior</p> : ""}
+                {currentStep !== QUESTIONS.length -1 ? <p onClick={handleNextQuestionClick}>Siguiente</p> : ""}
             </div>
         </div>
     );
