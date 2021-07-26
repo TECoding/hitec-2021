088f0c5a712f7e2d44d492c1abe48d2c7867a127
AlonsoGarcia255 <71342528+AlonsoGarcia255@users.noreply.github.com>
2021-07-25 19:59:21 -0500

    Result screen added
    - Result screen added with example information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,19 +15,20 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <img src="logo_hitec.svg" alt="hitec" className="hitec-logo"/>
         <Switch>
           <Route path="/" exact>
+            <img src="logo_hitec.svg" alt="hitec" className="hitec-logo"/>
             <Inicio/>
             <Link to="/preguntas" className="btn-start">Iniciar</Link>
           </Route>
           <Route path="/preguntas">
             <Preguntas/>
-            <Link to="/">Regresar a Inicio</Link>
+            <Link to="/" className="btn-back-to-home">Ir a Inicio</Link>
+            <Link to="/resultado">Resultado</Link>
           </Route>
           <Route path="/resultado">
             <Resultado/>
-            <Link to="/">Regresar a Inicio</Link>
+            <Link to="/" className="btn-back-to-home btn-back-to-home--resultado">Ir a Inicio</Link>
           </Route>
         </Switch>
         <footer>
