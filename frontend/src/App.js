b33f01fa968635790f6e540cd0abd2204fe58fc2
AlonsoGarcia255 <71342528+AlonsoGarcia255@users.noreply.github.com>
2021-07-26 23:00:01 -0500

    Fixed links
    - Moved to each component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,18 +17,13 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact>
-            <img src="logo_hitec.svg" alt="hitec" className="hitec-logo"/>
             <Inicio/>
-            <Link to="/preguntas" className="btn-start">Iniciar</Link>
           </Route>
           <Route path="/preguntas">
             <Preguntas/>
-            <Link to="/" className="btn-back-to-home">Ir a Inicio</Link>
-            <Link to="/resultado">Resultado</Link>
           </Route>
           <Route path="/resultado">
             <Resultado/>
-            <Link to="/" className="btn-back-to-home btn-back-to-home--resultado">Ir a Inicio</Link>
           </Route>
         </Switch>
         <footer>
